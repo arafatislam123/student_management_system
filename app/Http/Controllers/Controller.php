c47a5bfba2685b67ec455af1b900ba1855056bcd
Arafat420 <arafatpstu7@gmail.com>
2025-04-02 19:48:08 +0600

    Your descriptive commit message here

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,12 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function __construct()
+    {
+        // Initialize any common properties or methods for all controllers here
+    }
+    public function __destruct()
+    {
+        // Clean up resources or perform any final actions before the object is destroyed
+    }
 }
